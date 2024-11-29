c4fccc97ae65acccf6b052cb932079c41e378829
Marcus <marcusmah6969@gmail.com>
2024-11-29 18:03:06 +0800

    Remove Waring

--- lib/pages/checkout.dart
+++ lib/pages/checkout.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:um_internal_hackathon/home_page.dart';
 import 'package:um_internal_hackathon/pages/checkout_message.dart';
 
